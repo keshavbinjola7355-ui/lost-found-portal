f07873cc2e1b2995902d3442fa7b1d01fd124160
keshavbinjola7355-ui <keshavbinjola7355@gmail.com>
2026-06-15 14:46:51 +0530

    Add login page and prototype authentication

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+
+function Login() {
+  const [rollNo, setRollNo] = useState("");
+  const [password, setPassword] = useState("");
+
+  const demoUser = {
+    rollNo: "2301234",
+    password: "keshav123",
+  };
+
+  function handleLogin() {
+    if (
+      rollNo === demoUser.rollNo &&
+      password === demoUser.password
+    ) {
+      alert("Login Successful!");
+    } else {
+      alert("Invalid Credentials!");
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1>College Lost & Found</h1>
+        <p>Find. Report. Return.</p>
+
+        <input
+          type="text"
+          placeholder="College Roll Number"
+          value={rollNo}
+          onChange={(e) => setRollNo(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) =>
+            setPassword(e.target.value)
+          }
+        />
+
+        <button onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
